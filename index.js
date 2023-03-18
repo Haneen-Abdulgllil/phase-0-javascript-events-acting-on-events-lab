e5741ca6527d1994d4f2e9d17132f376dcb35105
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-03-18 11:34:44 +0300

    assignment complated

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") 
+      moveDodgerLeft();
+
+    if(e.key === "ArrowRight")
+      moveDodgerRight();
+});
+function moveDodgerLeft() {
+  const coordinate_px = dodger.style.left.replace("px", "");
+  const coordinate = parseInt(coordinate_px, 10);
+
+  if (coordinate > 0) {
+    dodger.style.left = `${coordinate - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  const coordinate_px = dodger.style.left.replace("px", "");
+  const coordinate = parseInt(coordinate_px, 10);
+
+
+  if (coordinate < 360) {
+    dodger.style.left = `${coordinate + 1}px`;
+  }
+}
